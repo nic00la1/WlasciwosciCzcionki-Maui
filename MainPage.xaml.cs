6379ba9de127bfdb835c34d3a494dcaefb5a9301
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-14 14:46:16 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace WlasciwosciCzcionki
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private string[] quotes = { "Dzień dobry", "Good morning", "Buenos dias" };
+        private int currentQuoteIndex = 0;
+
+
+        private double sliderValue;
+        public double SliderValue
+        {
+            get { return sliderValue; }
+            set
+            {
+                if (sliderValue != value)
+                {
+                    sliderValue = value;
+                    OnPropertyChanged(nameof(SliderValue));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            SliderValue = value;
+            label.FontSize = value;
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            currentQuoteIndex = (currentQuoteIndex + 1) % quotes.Length;
+            label.Text = quotes[currentQuoteIndex];
+        }
+    }
+}
